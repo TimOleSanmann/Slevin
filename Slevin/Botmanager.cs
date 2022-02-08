d75f80b89a60ca1e7e953fa0992599dc1e0812b5
Blade-Tim\Tim <sanmannt@gmail.com>
2022-02-08 21:12:54 +0100

    Added Basic functions to start and login bot

---
+++ Slevin/Botmanager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Discord.Commands;
+using Discord;
+
+namespace Slevin
+{
+    public class Botmanager
+    {
+        public DiscordSocketClient BotClient;
+        public CommandService CommandService;
+        public IServiceProvider ServiceProvider;
+        public const string PREFIX = "!";
+
+        public async Task RunBot()
+        {
+            CommandService = new CommandService();
+            ServiceProvider = ConfigureServices();
+            BotClient = new DiscordSocketClient();
+            await BotClient.LoginAsync(Discord.TokenType.Bot, Secret.GetToken());
+            await BotClient.StartAsync();
+
+            BotClient.Log += Log;
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public IServiceProvider ConfigureServices()
+        {
+            return null;
+        }
+    }
+}
